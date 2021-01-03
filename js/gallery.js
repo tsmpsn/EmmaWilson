14f3859b17161321eea663639949bf8eeda536dc
nospmismot <tomsimpson11@hotmail.co.uk>
2021-01-03 14:58:22 +0000

    gallery js and css to camel
    - Gallery js update
    - CSS renaming

--- js/gallery.js
+++ js/gallery.js
@@ -1,38 +1,39 @@
 /* Display pop-up */
 
 var imageMap = {
-	1: "img/GALLERY 1.jpg",
-	2: "img/GALLERY 2.jpg",
-	3: "img/GALLERY 3.jpg",
-	4: "img/GALLERY 4.jpg",
-	5: "img/GALLERY 5.jpg",
-	6: "img/GALLERY 6.jpg",
-	7: "img/GALLERY 7.jpeg",
-	8: "img/GALLERY 8.jpg",
-	9: "img/GALLERY 9.jpg",
-	10: "img/GALLERY 10.jpg",
-	11: "img/GALLERY 11.jpg",
-	12: "img/GALLERY 12.jpg",
-	13: "img/GALLERY 13.jpg",
-	14: "img/GALLERY 14.jpg",
-	15: "img/GALLERY 15.jpg",
-	16: "img/GALLERY 16.jpg",
-	17: "img/GALLERY 17.jpg",
-	18: "img/GALLERY 18.jpg"
+	1: "img/GALLERY1.jpg",
+	2: "img/GALLERY2.jpg",
+	3: "img/GALLERY3.jpg",
+	4: "img/GALLERY4.jpg",
+	5: "img/GALLERY5.jpg",
+	6: "img/GALLERY6.jpg",
+	7: "img/GALLERY7.jpeg",
+	8: "img/GALLERY8.jpg",
+	9: "img/GALLERY9.jpg",
+	10: "img/GALLERY10.jpg",
+	11: "img/GALLERY11.jpg",
+	12: "img/GALLERY12.jpg",
+	13: "img/GALLERY13.jpg",
+	14: "img/GALLERY14.jpg",
+	15: "img/GALLERY15.jpg",
+	16: "img/GALLERY16.jpg",
+	17: "img/GALLERY17.jpg",
+	18: "img/GALLERY18.jpg"
 };
 
 var currentImage = 0;
 
-// Left and right key press
-
-$("html").keydown(function(e) {
-	if(e.keyCode == 37) { // left
-		console.log("left");
-		previousImage();
-	}
-	else if(e.keyCode == 39) { // right
-		nextImage();
-		console.log("right");
+document.addEventListener("keydown", function(e) {
+	switch(expression) {
+		case 37:
+			previousImage();
+			break;
+		case 39:
+			nextImage();
+			break;
+		case 27:
+			hideFullImage();
+			break;
 	}
 });
 
