3289a2a7d750209a6a6dc419fc5db493a57c44fa
nospmismot <tomsimpson11@hotmail.co.uk>
2021-01-03 11:39:10 +0000

    Updated .gitignore

--- js/server.js
+++ js/server.js
@@ -9,7 +9,8 @@ var mimeTypes = {
     "jpg": "image/jpeg",
     "png": "image/png",
     "js": "text/javascript",
-    "css": "text/css"
+    "css": "text/css",
+    "gif": "image/gif"
 };
 
 http.createServer((request, response) => {
